4a879220cab9d8a333d3e79eba3cf383384c6304
Harpreet. <roopraharpreet1@gmail.com>
2025-03-18 22:35:42 +0000

    Update ytDownloader.py

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,4 +1,4 @@
-from pytube import YouTube
+from pytubefix import YouTube
 from sys import argv
 
 #takes all of the things you input into the command line so [1] takes the first line in command
@@ -7,3 +7,6 @@ yt = YouTube(link)
 
 print("Title: ", yt.title)
 
+ytVidDownload = yt.streams.get_highest_resolution()
+
+ytVidDownload.download('C:/Users/Aman/Videos/Download YT Videos')
